d331971cdc8af1957c0a36e89a2f939dcea1e58e
drawn <fqhua_5@sina.cn>
2018-05-31 09:58:47 +0800

    'update'

---
+++ precache-manifest.6d25212533b422fae757d50f4ee0b0e3.js
@@ -0,0 +1,24 @@
+self.__precacheManifest = [
+  {
+    "url": "/js/common.bundle.180bf71db02907105f43.js"
+  },
+  {
+    "url": "/js/app.bundle.8f6b2e35ef25ab44ed20.js"
+  },
+  {
+    "url": "/js/another.bundle.7c09d6bc9311c4027baa.js"
+  },
+  {
+    "revision": "b591d0ab7e9def9a8143cf74aaa31685",
+    "url": "/index.html"
+  },
+  {
+    "url": "/img/5b1e3a536c7bb0df157fa9963dd303a0.jpg"
+  },
+  {
+    "url": "/fonts/d41d8cd98f00b204e9800998ecf8427e.woff2"
+  },
+  {
+    "url": "/fonts/d41d8cd98f00b204e9800998ecf8427e.woff"
+  }
+];
